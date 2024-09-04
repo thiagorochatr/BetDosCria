6749c32ad2d418fa4478915c1aa79d08f9938188
thiagorochatr <thiagord2016@gmail.com>
2024-09-04 15:31:36 -0300

    TailwindCSS Set Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 import WebApp from '@twa-dev/sdk'
 
@@ -10,22 +7,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
       </div>
         {/* Here we add our button with alert callback */}
-      <div className="card">
+      <div className="">
         <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
             Show Alert
         </button>
